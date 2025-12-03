a838a3db653b00ae40d1bd292474e7db7bf43697
Patrice Philetas <patricephiletas@Host-002.lan>
2025-12-03 15:25:46 +0100

    correction titre page

--- src/main.js
+++ src/main.js
@@ -2,59 +2,52 @@
 // import { displayEvents } from "./event.js";
 // / import { affichage_element } from "./dom.js";
 
+// --------------------------------------------------------------------
 
 async function recupererDonnees() {
   try {
     const response = await fetch(
       'https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/que-faire-a-paris-/records?limit=20'
     );
-
     const data = await response.json();
-
     return data.results;
-
   } catch (error) {
     console.error('Erreur:', error);
   }
 }
+// --------------------------------------------------------------------
 
 async function afficherDonnees() {
+  
   const evenements = await recupererDonnees();
-
   const conteneur = document.createElement('div');
-
   let html = '';
-
-  evenements.forEach(item => {
+  evenements.forEach(data => {
     html += `
       <article>
-        <h2>${item.title}</h2>
-
+        <h2>${data.title}</h2>
         <img 
-          src="${item.cover_url}" 
-          alt="${item.title}" 
-          width="300"
-        >
-
-        <p><strong>Date :</strong> ${item.date_start ?? 'Non communiqué'}</p>
-
+          src="${data.cover_url}" 
+          alt="${data.title}" 
+          width="300">
+        <p><strong>Date :</strong> ${data.date_start ?? 'Non communiqué'}</p>
         <p>
           <strong>Lieu :</strong> 
-          ${item.address_name ?? ''} 
-          ${item.address_street ?? ''} 
-          ${item.address_zipcode ?? ''} 
-          ${item.address_city ?? ''}
+          ${data.address_name ?? ''} 
+          ${data.address_street ?? ''}  
+          ${data.address_zipcode ?? ''} 
+          ${data.address_city ?? ''}
         </p>
-        <p><strong>Description: </strong> ${item.lead_text ?? 'Non communiqué'} <br>${item.description}</p>
-
-        <p><strong>Contact :</strong> ${item.contact_mail ?? 'Non communiqué'}</p>
+        <p><strong>Description: </strong> ${data.lead_text ?? 'Non communiqué'} <br>${data.description}</p>
+        <p><strong>Mail :</strong> ${data.contact_mail ?? 'Non communiqué'}</p>
       </article>
     `;
   });
-
-  conteneur.innerHTML = html;
-
+  conteneur.innerHTML = `<h1>Adataviz – Événements Paris</h1><br>${html}`;
   document.body.appendChild(conteneur);
 }
-
 afficherDonnees();
+
+// --------------------------------------------------------------------
+
+
