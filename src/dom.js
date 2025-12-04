4d3b54a549d939699646be97e0f5b9b01fd9fa42
Patrice Philetas <patricephiletas@Host-002.lan>
2025-12-04 22:57:27 +0100

    bouton voirPlus / limitation à 3 résultat

--- src/dom.js
+++ src/dom.js
@@ -1,12 +1,10 @@
 
 
-
-
 export function afficherCards(data) {
 return `
     <article>
       <h2>${data.title}</h2>
-      <img src="${data.cover_url}" alt="Event titled ${data.title} featuring a visual representation related to the event. The image showcases the atmosphere of the event, highlighting key elements that convey its theme." width="300">
+      <img src="${data.cover_url}" alt="Event titled ${data.title} " width="300">
       <p><strong>Date :</strong> ${data.date_start ?? 'Non communiqué'}</p>
       <p>
         <strong>Lieu :</strong> 
--- src/main.js
+++ src/main.js
@@ -1,34 +1,57 @@
 
 
 import { recupererDonnees } from './event.js';
-// import { afficherDonnees } from './dom.js';
 import { afficherCards } from "./dom.js";
 
 
 
 let dataTotal = [];
+let currentIndex = 0;
+const limit = 3;
 
 async function afficherDonnees() {
-  const evenements = await recupererDonnees();
-  const conteneur = document.getElementById('events-container');
-  let html = [];
-  evenements.forEach(data => {
-    html.push(afficherCards(data)); 
-  });
-  conteneur.innerHTML = `<h1>Adataviz – Événements Paris</h1><br>
-  <input 
-  type="text" 
-  id="search"
-  style="width: 80%; margin-bottom: 20px; font-size: 16px;" 
-  placeholder="Rechercher..."><br>${html.join('')}`;
+  dataTotal = await recupererDonnees(); 
+  initialiserPage();
+  afficherMorceaux(); 
 }
 
-afficherDonnees()
-// // --------------------------------------------------------------------
+  
 
-// async function afficherCards() {
+function initialiserPage() {
+  const conteneur = document.getElementById('events-container');
+
+  conteneur.innerHTML = `
+    <h1>Adataviz – Événements Paris</h1><br>
+
+    <input 
+      type="text" 
+      id="search"
+      style="width: 80%; margin-bottom: 20px; font-size: 16px;" 
+      placeholder="Rechercher..."><br>
+
+    <div id="cards-list"></div>
+
+    <button id="voirPlus">Voir plus</button>
+  `;
+
+document.getElementById("voirPlus").addEventListener("click", afficherMorceaux);
+}
 
 
+function afficherMorceaux() {
+  const target = document.getElementById("cards-list");
 
-// }
+  const slice = dataTotal.slice(currentIndex, currentIndex + limit);
 
+  slice.forEach(data => {
+    target.innerHTML += afficherCards(data);
+  });
+
+  currentIndex += limit;
+
+  // Si plus rien à afficher → on cache le bouton
+  if (currentIndex >= dataTotal.length) {
+    document.getElementById("voirPlus").style.display = "none";
+  }
+}
+afficherDonnees();
